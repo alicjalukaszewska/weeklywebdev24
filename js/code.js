d6cc66a85d5159d74e792956e9fc0f33f9268469
Alicja <alicja.x.lukaszewska@gmail.com>
2017-12-28 00:24:46 +0100

    Initial commit with flexbox

---
+++ js/code.js
@@ -0,0 +1,17 @@
+
+//fixed nav
+
+// const nav = document.querySelector('.mainHeader__navig');
+// const topOfNav = nav.offsetTop;
+
+// function fixNav () {
+//   if ( window.scrollY >= topOfNav){
+//     document.body.style.paddingTop = nav.offsetHeight + 'px';
+//     document.body.classList.add('fixed-nav');
+//   } else {
+//     document.body.style.paddingTop = 0;
+//     document.body.classList.remove('fixed-nav');
+//   }
+// }
+
+// window.addEventListener('scroll', fixNav);

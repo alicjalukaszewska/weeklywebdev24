3cd3d55111b7f8bf34c53687ab63a42e41abad91
Alicja <alicja.x.lukaszewska@gmail.com>
2018-01-02 16:42:34 +0100

    before changed changeItemInTime function

--- js/code.js
+++ js/code.js
@@ -18,6 +18,13 @@ const element = document.querySelectorAll('li');
 element.forEach(li => li.addEventListener('click', changeActive));
 window.addEventListener('scroll', changeMenu);
 
+function getItemPosition () {
+	for(let i = 0; i < links.length; i++) {
+		const currentWidth = document.querySelector('.content').offsetWidth;
+		const position = currentWidth * i;
+		links[i].dataset.pos = `-${position}px`;
+	} 
+};
 
 function changeTrendingItem () {
 	const currentPosition = this.getAttribute("data-pos");
@@ -28,3 +35,26 @@ function changeTrendingItem () {
 const links = document.querySelectorAll('.chooseItem li');
 links.forEach(li => li.addEventListener('click', changeTrendingItem));
 
+
+function changeItemInTime () {
+	let lastItem = document.querySelector('.chooseItem').lastChild;
+	let firtsItem = document.querySelector('.chooseItem').firtsChild;
+	setInterval(function(){
+		links.forEach(li => {
+			let active = li.classList.contains('active');
+			if (active && li !== lastItem) {
+				li.nextElementSibling.classList.add('active');
+				li.nextElementSibling.click();							
+			}
+			if (li.classList.contains('active') && li !== firstItem) {
+				li.previousElementSibling.classList.add('active');
+				li.previousElementSibling.click();							
+			}
+			li.classList.remove('active');
+		});
+	}, 1000);
+}
+
+window.addEventListener('load', getItemPosition);
+window.addEventListener('resize', getItemPosition);
+window.addEventListener('load', changeItemInTime);
